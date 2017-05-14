26805c7e248b1936100123c394566e91102b381f
Sinan Akpolat <aciliketcap@gmail.com>
2017-05-14 20:53:07 +0300

    Added assertions and negative code for iw commands

--- periodic_scan.py
+++ periodic_scan.py
@@ -15,12 +15,19 @@ def station_dump_cmd(wlif):
     return "sudo iw dev " + wlif + " station dump"
 
 def cmd_run(cmd_string):
-    result = subprocess.run(cmd_string.split(' '), stdout=subprocess.PIPE, check=True)
-    return result.stdout.decode("utf-8")
+    try:
+        result = subprocess.run(cmd_string.split(' '), stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE, check=True)
+        return result.stdout.decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        raise e
+
 
 match_mac = r"([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})"
 match_power = r"((-)?\d+) dBm"
 
+match_link_failed = r"Not connected."
+match_iw_no_device = r"^command failed: No such device"
 match_bssid_from_link = r"Connected to (" + match_mac + ")"
 #writing regex for SSID can be quite tricky, just read until EOL
 match_ssid_from_link = r"\s+SSID:\s+(.*)$"
@@ -43,23 +50,21 @@ class ConnInfo:
                 "Signal: " + self.signal,
                 "Avg Signal: " + self.avg_signal])
 
-#throw instead of returning None in negative code
 def parse_current_connection_info(link_output, station_dump_output):
+    assert re.match(match_link_failed, link_output) is None, "Not connected"
+    assert len(station_dump_output) > 0, "Not connected"
+
     res_bssid_from_link = re.match(match_bssid_from_link, link_output)
-    if res_bssid_from_link is not None:
-        bssid_from_link = res_bssid_from_link.groups()[0]
-    else:
-        raise ValueError("No bssid from link command")
+    assert res_bssid_from_link is not None, "No bssid from link command"
+    bssid_from_link = res_bssid_from_link.groups()[0]
 
     res_bssid_from_station_dump = re.match(match_bssid_from_station_dump,
                                        station_dump_output)
-    if res_bssid_from_station_dump is not None:
-        bssid_from_station_dump = res_bssid_from_station_dump.groups()[0]
-    else:
-        raise ValueError("No bssid from station dump command")
+    assert res_bssid_from_station_dump is not None, "No bssid from station dump command"
+    bssid_from_station_dump = res_bssid_from_station_dump.groups()[0]
 
     if bssid_from_station_dump != bssid_from_link:
-        raise ValueError("BSSID's from commands does not match!")
+        raise ValueError("BSSID's from two commands does not match!")
 
     for l in link_output.splitlines()[1:]:
         res_ssid = re.match(match_ssid_from_link, l)
@@ -70,14 +75,8 @@ def parse_current_connection_info(link_output, station_dump_output):
         if res_freq is not None:
             freq = res_freq.groups()[0]
 
-    try:
-        ssid
-    except:
-        raise ValueError("No ssid")
-    try:
-        freq
-    except:
-        raise ValueError("No freq")
+    assert ssid, "No ssid"
+    assert freq, "No freq"
 
     for l in station_dump_output.splitlines()[1:]:
         res_signal = re.match(match_signal, l)
@@ -88,26 +87,29 @@ def parse_current_connection_info(link_output, station_dump_output):
         if res_avg_signal:
             avg_signal = res_avg_signal.groups()[0]
 
-    try:
-        signal
-    except:
-        raise ValueError("No signal")
-    try:
-        avg_signal
-    except:
-        raise ValueError("No avg signal")
+    assert signal, "No signal"
+    assert avg_signal, "No average signal"
 
     return ConnInfo(bssid_from_link, ssid, freq, signal, avg_signal)
 
 def gather_current_connection_info(wlif):
-    link_out = cmd_run(link_cmd(my_wlif))
-    station_dump_out = cmd_run(station_dump_cmd(my_wlif))
+    try:
+        link_out = cmd_run(link_cmd(my_wlif))
+        station_dump_out = cmd_run(station_dump_cmd(my_wlif))
+    except subprocess.CalledProcessError as e:
+        print("iw command failed:", " ".join(e.cmd))
+        if re.match(match_iw_no_device, e.stderr.decode("utf-8")):
+            print("Device", wlif, "not found!")
+        else:
+            print("With output:", e.stderr)
+        return
 
     return parse_current_connection_info(link_out, station_dump_out)
 
-
 #control output
-print(gather_current_connection_info(my_wlif))
+cur_conn_info = gather_current_connection_info(my_wlif)
+if cur_conn_info is not None:
+    print(cur_conn_info)
 
 
 
